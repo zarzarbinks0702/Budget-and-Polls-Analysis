49853ae76241d6e5808e920413aa2de0aebf66ad
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-03-19 20:55:11 -0500

    Update main.py
    added code to find the total profits/losses in the data sheet

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,14 +6,20 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 
 #set the initial row count to 0 to determine the total number of months
 row_count = 0
+#set the initial value fro total profits
+total_profits = 0
 
 #open csv data file for pybank project and make it readable
 with open (csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    #adds one to the row count for each row
+    #for each row in the csv file
     for row in csvreader:
+        #adds one to the row count for each row
         row_count += 1
+        #adds the profits/losses to previous total
+        total_profits += int(row[1])
+
 
 #print total months as rows minus 1 since one of the rows is the header row
 print(f"Total Months: {row_count - 1}")
