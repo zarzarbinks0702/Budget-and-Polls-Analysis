fecbd834b1820fe5c38e46d2354c28e8d1cfd444
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-03-21 15:05:22 -0500

    Update main.py
    added the code to find the change in profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,17 +2,19 @@ import os
 import csv
 
 #link to the csv data file for pybank project
-csvpath = os.path.join("Resources", "budget_data.csv")
+pybank_csv = os.path.join("Resources", "budget_data.csv")
 
 #set the initial row count to 0 to determine the total number of months
 row_count = 0
-#set the initial value fro total profits
+#set the initial value for total profits
 total_profits = 0
 #array for the values of the profits/losses column
 profits_and_losses = []
+#array for the changes in profits and Losses
+change_in_profits = []
 
 #open csv data file for pybank project and make it readable
-with open (csvpath, newline='') as csvfile:
+with open (pybank_csv, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #pulls header row out
     csv_header = next(csvreader)
@@ -23,8 +25,20 @@ with open (csvpath, newline='') as csvfile:
         #adds the profits/losses to previous total
         total_profits += int(row[1])
         #appends each value in the profits/losses column to the profits_and_losses array
-        profits_and_losses.append(int(row[1]))
+        profits_and_losses.append(row[1])
+
+#creating amended lists of profits_and losses - one without the first value and one without the last value to zip and calculate the change in profits
+profits_and_losses_no_first = profits_and_losses[1:]
+profits_and_losses_no_last = profits_and_losses[:-1]
+
+#zip the new profits_and_losses lists together to form one list - and converting it to proper form
+new_profits_and_losses = list(zip(profits_and_losses_no_last, profits_and_losses_no_first))
+
+for set in new_profits_and_losses:
+    change = int(set[0]) + int(set[1])
+    change_in_profits.append(int(change))
 
 #print total months as rows
 print(f"Total Months: {row_count}")
+#prints the net profits
 print(f"Net Profits: ${total_profits}")
