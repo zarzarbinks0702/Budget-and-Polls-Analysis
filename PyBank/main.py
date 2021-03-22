2c3f6e343663538bb6f4a9479604ea7d59ab01ab
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-03-22 14:55:13 -0500

    Update main.py
    added the calculation for average change in profits, syntax to print the value

--- PyBank/main.py
+++ PyBank/main.py
@@ -34,11 +34,25 @@ profits_and_losses_no_last = profits_and_losses[:-1]
 #zip the new profits_and_losses lists together to form one list - and converting it to proper form
 new_profits_and_losses = list(zip(profits_and_losses_no_last, profits_and_losses_no_first))
 
+#goes through the new_profits_and losses list
 for set in new_profits_and_losses:
+    #calculates the change between the values stored in each list item
     change = int(set[1]) - int(set[0])
+    #adds the new value to the change in profits list
     change_in_profits.append(int(change))
+
+#takes the change_in profits list
+for change in change_in_profits:
+    #set a starting variable to hold the sum of all the changes
+    sum_of_changes = 0
+    #add each value together
+    sum_of_changes += int(change)
+    #divide the sum by the length on the change_in_profits list to find the average change
+    avg_change = sum_of_changes / len(change_in_profits)
 
 #print total months as rows
 print(f"Total Months: {row_count}")
 #prints the net profits
 print(f"Net Profits: ${total_profits}")
+#prints the average change in profits/Losses, rounded to 2 decimal places (since we're dealing with money)
+print(f"Average Change: ${round(avg_change, 2)}")
