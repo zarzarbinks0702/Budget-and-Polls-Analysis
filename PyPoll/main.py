b583ae5d63d9c7197ab1899507602aa1201cdfdf
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-03-22 19:25:14 -0500

    Update main.py
    bug fix, added the print function

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,10 +5,11 @@ import csv
 csvpath = os.path.join("Resources", "election_data.csv")
 
 #set initial value for row count, used to find the total number of votes cast
-vote_count = 0
+total_vote_count = 0
 #set initial value for the candidate list
 candidates = []
-
+#stores the third column as a list
+all_vote_names = []
 #open csv data file for pybank project and make it readable
 with open (csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -17,7 +18,7 @@ with open (csvpath, newline='') as csvfile:
     #for each row in the csv file
     for row in csvreader:
         #adds one to the vote count for each row (one row = one vote)
-        vote_count += 1
+        total_vote_count += 1
         #checks the value of the 3rd column is in the candidates list
         if row[2] in candidates:
             #does nothing if the name is already in the list
@@ -26,14 +27,19 @@ with open (csvpath, newline='') as csvfile:
         else:
             #adds the name to the candidates list
             candidates.append(row[2])
-        #function for determining total votes and percentage of votes won by each candidate - takes the index of the candidates list as a parameter
-        def vote_count(candidate_index)
-            #count how many times a candidate's name appears in the 3rd column
-            candidate_votes = row[2].count(candidates[candidate_index])
-            #uses vote_count and candidate_votes to find the percentage of votes
-            percentage_votes = int(candidate_votes) / int(vote_count) * 100
-            #prints the information to the console
-            print(f"{candidates[candidate_index]}: {round(percentage_votes, 3)}% ({candidate_votes})")
+        #adds each item in the third column to the all_vote_names list
+        all_vote_names.append(row[2])
+    #function for determining total votes and percentage of votes won by each candidate - takes the index of the candidates list as a parameter
+    def vote_count(candidate_index):
+        #count how many times a candidate's name appears in the 3rd column (stored in the all_vote_names list)
+        candidate_votes = all_vote_names.count(candidates[candidate_index])
+        #uses vote_count and candidate_votes to find the percentage of votes
+        percentage_votes = candidate_votes / total_vote_count * 100
+        #prints the information to the console
+        print(f"{candidates[candidate_index]}: {round(percentage_votes, 3)}% ({candidate_votes})")
 
 #prints the total number of votes cast
-print(f"Total Votes: {vote_count}")
+print(f"Total Votes: {total_vote_count}")
+#prints the vote_count function for each candidate in the candidates list
+for each in range(len(candidates)):
+    print(vote_count(each))
