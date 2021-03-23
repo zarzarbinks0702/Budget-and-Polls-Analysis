f76dec97e121aed7386f417f51d368ae47b27ce8
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-03-22 19:12:37 -0500

    Update main.py
    added the function to find total votes and percentage of votes for each candidate then print the information

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,6 +26,14 @@ with open (csvpath, newline='') as csvfile:
         else:
             #adds the name to the candidates list
             candidates.append(row[2])
+        #function for determining total votes and percentage of votes won by each candidate - takes the index of the candidates list as a parameter
+        def vote_count(candidate_index)
+            #count how many times a candidate's name appears in the 3rd column
+            candidate_votes = row[2].count(candidates[candidate_index])
+            #uses vote_count and candidate_votes to find the percentage of votes
+            percentage_votes = int(candidate_votes) / int(vote_count) * 100
+            #prints the information to the console
+            print(f"{candidates[candidate_index]}: {round(percentage_votes, 3)}% ({candidate_votes})")
 
 #prints the total number of votes cast
-print(f"Total Votes Cast: {vote_count}")
+print(f"Total Votes: {vote_count}")
